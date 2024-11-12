dddeadca0d7c3b5c17751994be1dd135166161bc
Charity Everett <155777861+CharityEverett@users.noreply.github.com>
2024-11-11 18:10:22 -0600

    Update fluid-frame.js
    Updated scale function

--- components/fluid-frame.js
+++ components/fluid-frame.js
@@ -15,14 +15,23 @@ AFRAME.registerComponent('fluid-frame', {
     window.removeEventListener('resize', this.updateScale);
   },
 
- updateScale: function() {
-  var data = this.data;
-  var el = this.el;
-  
-  var width = window.innerWidth;
-  var scale = Math.max(data.minScale, Math.min(width / data.scaleThreshold, data.maxScale));
-  
-  el.object3D.scale.set(scale, scale, scale);
-}
+updateScale: function() {
+    var data = this.data;
+    var el = this.el;
+    
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var aspectRatio = width / height;
+    
+    // Calculate scale based on both width and aspect ratio
+    var widthScale = Math.max(data.minScale, Math.min(width / data.scaleThreshold, data.maxScale));
+    var aspectScale = Math.max(data.minScale, Math.min(aspectRatio, data.maxScale));
+    
+    var finalScale = (widthScale + aspectScale) / 2;
+    
+    el.object3D.scale.set(finalScale, finalScale, finalScale);
+    
+    // Emit an event for other components to react to scale changes
+    el.emit('scaleChanged', {scale: finalScale});
   }
 });
